13ae02ccbd4f62cc58d44aa5969398ac2b846502
MarcL13 <42978461+MarcL13@users.noreply.github.com>
2018-09-17 21:45:31 -0400

    Add files via upload

---
+++ src/CalculatorSub.java
@@ -0,0 +1,10 @@
+
+public class CalculatorSub
+{
+	
+	public int subtract(int num1,int num2)
+	{
+		int ans = num1-num2;
+		return ans;
+	}
+}
---
+++ src/CalculatorSubtract.java
@@ -0,0 +1,17 @@
+
+/**
+ * 
+ * @author Marc Lussier, Period 7
+ * Calculator- subtraction
+ *
+ */
+
+public class CalculatorSubtract
+{
+	public static void main(String [] args)
+	{
+		CalculatorSub subtract = new CalculatorSub();
+		System.out.print(subtract.subtract(20,5));
+	}
+}
+
